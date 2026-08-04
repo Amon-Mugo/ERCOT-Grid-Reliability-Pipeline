bd650827991427626f7013a8c1463f933b1d6cef
Amon-Mugo <amonkariuki325@gmail.com>
2026-08-04 22:55:08 +0300

    added a demand_transform file for pyspark it is used for etl data transformation and also format the date correctly incase of any suprises ,also enables data to be stored as database tables  using the error checks from the other files in src

---
+++ src/transform/demand_transform.py
@@ -0,0 +1,30 @@
+#pyspark etl
+
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col,lit,to_timestamp
+from src.transform.validators import  validators_required_columns_not_null,validators_no_parse_failures
+
+EIA_PERIOD_FORMAT="yyyy-MM-dd'T'HH"
+
+def transform_demand(df:DataFrame, ingestion_date:str) -> DataFrame:
+
+    with_parsed= df.withColumn("parsed_period",to_timestamp(col("period"),EIA_PERIOD_FORMAT)).withColumn("parsed_value",col("value").cast("double")
+    )
+    validators_no_parse_failures(with_parsed, "period", "parsed_period", "demand")
+    validators_no_parse_failures(with_parsed, "value", "parsed_value", "demand")
+
+    curated=with_parsed.withColumn("ingestion_date",lit(ingestion_date).cast("date")
+    ).select(
+        col("respondent"),
+        col("parsed_period").alias("period"),
+        col("parsed_value").alias("value"),
+        col("value-units").alias("value_units"),
+        col("ingestion_date"),
+    )
+
+    validators_required_columns_not_null(
+        curated,
+        required_columns=["respondent","period","ingestion_date"],
+        dataset_name="demand",
+    )
+    return curated
